d0c24e4dcb403ff6ff93e92a15f901a177c2b8df
Akash Shukla <shuklaa884@gmail.com>
2022-09-07 18:28:22 +0530

    sublist solution

--- golang/sublist/sublist.go
+++ golang/sublist/sublist.go
@@ -1,3 +1,38 @@
 package sublist
 
 type Relation = string
+
+func checkSubset(i int, a, b []int) bool {
+	lb := len(b)
+	for j := 0; j < lb; j++ {
+		if a[i] != b[j] {
+			return false
+		}
+		i += 1
+	}
+	return true
+}
+
+func Sublist(a, b []int) string {
+	la := len(a)
+	lb := len(b)
+
+	if la == lb {
+		if checkSubset(0, a, b) {
+			return "equal"
+		}
+	} else if la < lb {
+		for i := 0; i < lb; i++ {
+			if lb-i >= la && checkSubset(i, b, a) {
+				return "sublist"
+			}
+		}
+	} else {
+		for i := 0; i < la; i++ {
+			if la-i >= lb && checkSubset(i, a, b) {
+				return "superlist"
+			}
+		}
+	}
+	return "unequal"
+}
